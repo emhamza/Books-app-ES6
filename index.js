8a7938dd952718b785b0d4d8a5faed99494d56af
emhamza <imumairhamza@gmail.com>
2023-03-27 13:17:13 +0500

    Unused part of code remoced

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 import { DateTime } from './node_modules/luxon/src/luxon.js';
-
-// Get the current date and time
-
-// const now = DateTime.now().toLocaleString(DateTime.DATETIME_FULL);
-
-// Display the current date and time in the element with id "current-date-time"
-// document.getElementById('current-date-time').textContent = now;
-
-// .........................................=========
 import Library from './modules/library.js';
-
-/* NAVIGATION */
-
 import { initializeNavigation } from './modules/navigation.js';
 
+// function for current date and time
 const displayCurrentDateTime = () => {
   const now = DateTime.now().toLocaleString(DateTime.DATETIME_FULL);
   document.getElementById('current-date-time').textContent = now;
@@ -21,24 +10,13 @@ const displayCurrentDateTime = () => {
 
 displayCurrentDateTime();
 
+// For Library instances
 document.addEventListener('DOMContentLoaded', () => {
   const library = new Library();
   library.init();
 });
 
+// For initialization of the NAVIGATION bar
 document.addEventListener('DOMContentLoaded', () => {
   initializeNavigation();
 });
-
-// Current Date and Time
-
-// import { DateTime } from './node_modules/luxon/src/luxon.js';
-
-// function displayCurrentDate() {
-//   const currentDate = DateTime.now().toLocaleString(DateTime.DATE_FULL);
-//   const dateElement = document.createElement('p');
-//   dateElement.textContent = `Today's Date: ${currentDate}`;
-//   document.body.appendChild(dateElement);
-// }
-
-// displayCurrentDate();
